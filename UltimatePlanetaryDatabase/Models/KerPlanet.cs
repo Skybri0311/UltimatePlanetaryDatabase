01a362eb9accec479df6c970b82c6b685e3a71ca
Fiedler Jr <bfiedlerjr93@gmail.com>
2018-08-04 11:36:12 -0600

    fjlajk

---
+++ UltimatePlanetaryDatabase/Models/KerPlanet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UltimatePlanetaryDatabase.Models
+{
+    public class KerPlanet
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public string PlanetComp { get; set; }
+        public string EquatRad { get; set; }
+        public string EquatCirc { get; set; }
+        public string SurfaceArea { get; set; }
+        public string Mass { get; set; }
+        public string SGP { get; set; }
+        public string Density { get; set; }
+        public string SurfaceGrav { get; set; }
+        public string EscVel { get; set; }
+        public string RotPer { get; set; }
+        public string SyncOrbit { get; set; }
+        public string SoI { get; set; }
+        public bool HasAtm { get; set; }
+        public string AtmPress { get; set; }
+        public string ScaleHeight { get; set; }
+        public string AtmHeight { get; set; }
+        public string MinTemp { get; set; }
+        public string MaxTemp { get; set; }
+        public bool HasOxygen { get; set; }
+        public bool HasWater { get; set; }
+        public bool HasMoon { get; set; }
+        public bool CanSupportLife { get; set; }
+
+    }
+}
